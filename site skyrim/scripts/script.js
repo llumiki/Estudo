b46e460884dfb0a69bfcfc53b108423d53ec765a
llumiki <lucasmmichel@gmail.com>
2022-06-02 18:35:14 -0300

    first commit

---
+++ site skyrim/scripts/script.js
@@ -0,0 +1,6 @@
+const btnMobile = document.getElementById('btn-mobile')
+function toggleMenu() {
+  const nav = document.getElementById('nav')
+}
+btnMobile.addEventListener('click', toggleMenu)
+nav.classlist, toggle('active')
