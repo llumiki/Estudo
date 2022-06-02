f3db7ab39300ba9d23745342445f835c3ccd1e1f
llumiki <lucasmmichel@gmail.com>
2022-06-02 19:09:14 -0300

    estudos

--- site skyrim/scripts/script.js
+++
@@ -1,6 +0,0 @@
-const btnMobile = document.getElementById('btn-mobile')
-function toggleMenu() {
-  const nav = document.getElementById('nav')
-}
-btnMobile.addEventListener('click', toggleMenu)
-nav.classlist, toggle('active')
